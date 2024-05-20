f13774a1f043377f3e07c204897276f84b2ac2c5
syahrul hasbir <105841102121@student.unismuh.ac.id>
2024-05-20 14:53:16 +0800

    ok gas

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App .js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: "white",
+      justifyContent: 'center',
+      alignItems: 'flex-end',//baris
+      flexDirection: 'row',
+      marginBottom: 100,
+    }}>
+      <View style={{
+        backgroundColor: 'red',
+        width: 150,
+        height: 70,
+        borderRadius: 10,
+        marginRight: 10,
+      }}>
+        <Text style={{
+          color: 'white',
+          textAlign: 'center',
+          lineHeight: 80,
+          fontSize: 30,
+          fontWeight: 'bold',
+        }}>
+          Login
+        </Text>
+      </View>
+      <View style={{
+        backgroundColor: 'blue',
+        width: 150,
+        height: 70,
+        borderRadius: 10,
+        marginRight: 10,
+      }}>
+        <Text style={{
+          color: 'white',
+          textAlign: 'center',
+          lineHeight: 70,
+          fontSize: 30,
+          fontWeight: 'bold',
+        }}>
+          Register
+        </Text>
+      </View>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Text.js
@@ -0,0 +1,2 @@
+import { StyleSheet,Text,Image,Button } from "react-native";
+import {}
